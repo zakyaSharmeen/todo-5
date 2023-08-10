aff83bced7d1f5c4947dd5683012c43cc2800b84
zakya sharmeen <111156443+zakyaSharmeen@users.noreply.github.com>
2023-08-10 22:39:10 +0530

    Add files via upload

---
+++ todo--7/script.js
@@ -0,0 +1,52 @@
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+let items = getItems();
+
+
+
+
+function getItems(){
+    const value = localStorage.getItem("todo-test") || '[]';
+    return JSON.parse(value);
+}
+
+
+function setItems(items){
+    const itemsJSON = JSON.stringify(items);
+
+    localStorage.setItem("todo-test", itemsJSON);
+}
+
+function addItem(){
+    items.unshift({
+        description: "",
+        completed:false
+    });
+    setItems(items);
+    refreshList();
+}
+
+function refreshList(){
+    ITEMS_CONTAINER.innerHTML = "";
+
+    for (const item of items){
+        const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
+        const descriptionInput = itemElement.querySelector(".item-description");
+        const completedInput = itemElement.querySelector(".item-completed");
+
+        descriptionInput.value = item.description;
+        completedInput.checked = item.completed;
+
+        ITEMS_CONTAINER.append(itemElement);
+    }
+}
+
+ADD_BUTTON.addEventListener("click", () => {
+    addItem();
+});
+refreshList();
+
+
+/////////
